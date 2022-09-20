33e95b1fa65f827f1a654d8703ce00b47658d647
fpreviatti <44658304+fpreviatti@users.noreply.github.com>
2022-09-20 13:35:36 -0300

    Import List

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/cidade/CidadeController.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/cidade/CidadeController.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.http.*;
+import java.util.*;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
